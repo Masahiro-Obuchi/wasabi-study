c59f7b7083de4f521620c40187e771d0dcee3223
Masairo Obuchi <o.masahiro0328@gmail.com>
2025-11-24 17:46:05 +0900

    create Future

--- src/executor.rs
+++ src/executor.rs
@@ -1,4 +1,5 @@
 extern crate alloc;
+use crate::hpet::global_timestamp;
 use crate::info;
 use crate::result::Result;
 use crate::x86::busy_loop_hint;
@@ -6,6 +7,7 @@ use alloc::boxed::Box;
 use alloc::collections::VecDeque;
 use core::fmt::Debug;
 use core::future::Future;
+use core::future::Ready;
 use core::panic::Location;
 use core::pin::Pin;
 use core::ptr::null;
@@ -16,6 +18,7 @@ use core::task::Poll;
 use core::task::RawWaker;
 use core::task::RawWakerVTable;
 use core::task::Waker;
+use core::time::Duration;
 
 pub struct Task<T> {
     future: Pin<Box<dyn Future<Output = Result<T>>>>,
@@ -127,4 +130,25 @@ impl Future for Yield {
 }
 pub async fn yield_execution() {
     Yield::default().await
+}
+
+pub struct TimeoutFuture {
+    time_out: Duration,
+}
+impl TimeoutFuture {
+    pub fn new(duration: Duration) -> Self {
+        Self {
+            time_out: global_timestamp() + duration,
+        }
+    }
+}
+impl Future for TimeoutFuture {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, _: &mut Context) -> Poll<()> {
+        if self.time_out < global_timestamp() {
+            Poll::Ready(())
+        } else {
+            Poll::Pending
+        }
+    }
 }
